cd35da325c24d50b59a4c6282f6f2e095538371b
Pulkit <pulkit.gera@research.iiit.ac.in>
2020-11-16 18:49:48 +0530

    hyperplane updated

--- classifiers/hyperplane.py
+++ classifiers/hyperplane.py
@@ -7,47 +7,10 @@ from tqdm import tqdm
 import POIS_deepcrypt.goldwasser_micali as gm
 from POIS_deepcrypt.Naiive_Bayes.ArgMax import handler_A
 
-def getFeatureValues():
-	#Random generation
-	feature_values = []
-	mapping = []
-	num_features = 2
-	dist_values_feature = 2
 
-	for i in range(num_features):
-		mapping.append({})
-		values = []
-		for j in range(dist_values_feature):
-			rand_val = random.randint(0,1000)
-			values.append( rand_val )
-
-		feature_values.append(values)
-
-	ind = 0	
-	for f_list in feature_values:
-		idx = 0	
-		for val in f_list:
-			mapping[ ind ][ val ] = idx
-			idx += 1
-		ind += 1	
-
-	return 	feature_values , mapping	
-
-
-def genInput(mapping):
-	#random input generated
-	inp=[]
-	for values in mapping:
-		possible = []
-		for key in values:
-			possible.append(key)
-		inp.append(random.choice(possible))	
-	return inp	
 def paillier_enc(x,pub_key):
 	return paillier.encrypt( mpz(x) , pub_key)
 
-
-feature_values,mapping = getFeatureValues()
 # Input will be of the form 1*d
 inp_vec = [1330,1550]
 # Weights will be of the form d*k. Size of each array is d and number of arrays are k
@@ -81,7 +44,7 @@ Encrypting the weights
 """
 Server Side functions
 """
-def dot(a,b,key_pair):
+def dot(a,b,public_key):
 	"""
 	Dot product. Input is 2 unencrypted values and keys. 
 	Encrypt server side and then compute
@@ -90,32 +53,29 @@ def dot(a,b,key_pair):
 	vector_b_encrypt = []
 	for each in b:
 	    each = mpz(each)
-	    c1 = paillier.encrypt(each, key_pair.public_key)
+	    c1 = paillier.encrypt(each, public_key)
 	    vector_b_encrypt.append(c1)
-
 	fval = []
 	for ind,each in enumerate(vector_b_encrypt):
 	    c2 = mpz(a[ind])
 	    c1 = each
-	    # x = paillier.decrypt(c1 * c2, key_pair.private_key)
 	    x = c1*c2
 	    fval.append(x)
 	fsum = fval[0]
 	for val in fval[1:]:
 		fsum+=val
-	# return paillier.encrypt(fval,key_pair.public_key)
 	return fsum 
 
-def compute_dot(inp_vec,weights,key_pair):
+def compute_dot(inp_vec,weights,public_key):
 	encrypted_dot_product = []
 	for i in range(len(weights)):
-		output = dot(inp_vec,weights[i].tolist(),key_pair)
+		output = dot(inp_vec,weights[i].tolist(),public_key)
 		encrypted_dot_product.append(output)
 	return np.asarray(encrypted_dot_product)
 
-encrypted_vec = compute_dot(inp_vec,weights,key_pair)
-print(encrypted_vec)
+encrypted_vec = compute_dot(inp_vec,weights,key_pair.public_key)
+
 # argmax
-index = handler_A(encrypted_vec,10,key_pair.public_key,key_pair.private_key)
+index = handler_A(encrypted_vec,20,key_pair.public_key,key_pair.private_key)
 # return class
 print(index)
